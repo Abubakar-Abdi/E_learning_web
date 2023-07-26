78feb001d44a25d89a79d80f5c582a1167224be8
Abubakar Abdisalan Warsame <97209784+Abubakar-Abdi@users.noreply.github.com>
2023-07-26 07:53:59 +0600

    Delete myapp directory

--- myapp/Serializer.py
+++
@@ -1,5 +0,0 @@
-from rest_framework import serializers
-from .models import feature 
-class featureserializer (serializers.ModelSerializer):
-    model=(feature),
-    feilds=['id','username','email','University_name','country','DOG'],
